4ea28e5166a7efba9964598b5edb5b933f40b53d
tnt2017 <tnt2011@bk.ru>
2017-08-18 09:45:59 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace cons
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(21, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(295, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Личная папка";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(21, 194);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(597, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(21, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(295, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Консультант+ (версия проф)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(21, 227);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(597, 109);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Мы переехали на более мощный сервер и более современную систему. Прошу прощения з" +
+    "а временные неудобства !";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(322, 23);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(296, 37);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Реквизиты оплаты";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(322, 66);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(296, 37);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Помощь";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(322, 109);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(296, 42);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Выход";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(21, 66);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(295, 37);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Гарант-Эксперт (Омск)";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(21, 157);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(597, 26);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "По всем вопросам пишите tnt2011@bk.ru или звоните +79538000300 (Антон)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 359);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "TNT-NETS CLIENT v3.0 (build 29.07.2017)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+
+
+namespace cons
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll")]
+        static extern uint WinExec(string lpCmdLine, uint uCmdShow);
+
+        [DllImport("shell32.dll")]
+        private static extern int ShellExecute(int hWnd, string Operation, string File, string Parameters,
+                                           string Directory, int nShowCmd);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "Ваш логин: " + Environment.UserName;
+            if (Environment.UserName == "Администратор" || Environment.UserName == "User")
+            {
+                ShellExecute(0, "open", "explorer.exe", "", "C:\\WINDOWS", 1);
+                ///MessageBox.Show("Запустили рабочий стол");
+                Close();
+            }
+            else
+            {
+                string url = "http://192.168.1.100:888/dolg.php?login=" + Environment.UserName;
+
+                using (var client = new System.Net.WebClient())
+                {
+                    string sitetext = client.DownloadString(url);
+                    textBox2.Text = sitetext;
+                    if (sitetext.IndexOf("закончился")>0)
+                    {
+                        button2.Enabled = false;
+                        button6.Enabled = false;
+                        button1.Enabled = false;
+                        MessageBox.Show("Вышел срок оплаты. Пополните баланс !","Внимание", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("explorer.exe");
+            startInfo.UseShellExecute = true;
+            Process.Start(startInfo);
+            //Process.Start("explorer.exe");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string path1 = Directory.GetCurrentDirectory();
+            //MessageBox.Show(path1);
+            Environment.CurrentDirectory = ("D:\\Veda\\");
+
+            string path2 = Directory.GetCurrentDirectory();
+            //MessageBox.Show(path2);
+
+            ProcessStartInfo startInfo = new ProcessStartInfo("D:\\!!! SPS !!!\\Veda\\cons.exe");
+            startInfo.WorkingDirectory = "D:\\Veda\\";
+            Process.Start(startInfo);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
